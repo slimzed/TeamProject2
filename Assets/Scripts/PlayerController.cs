5c74a1483c7645e7608a62f05582d6c705bf135c
slimzed <cooldyde21@gmail.com>
2025-07-15 14:58:02 -0700

    spawner script created

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,13 +9,24 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) 
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("move right");
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("move right");
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             Debug.Log("high attack");
-        } else if (Input.GetKeyDown(KeyCode.Alpha2))
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             Debug.Log("mid attack");
-        } else if (Input.GetKeyDown(KeyCode.Alpha3))
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             Debug.Log("low attack");
         }
